558d95705b6e3d1410e700f0469e77ec70d903aa
gulpipose <guro.olsen@gmail.com>
2019-11-15 18:15:54 +0100

    Add statuscolumn to tasks table

--- src/main/resources/db/migration/V001__createTasks.sql
+++ src/main/resources/db/migration/V001__createTasks.sql
@@ -1,4 +1,12 @@
+CREATE TYPE STATUS AS ENUM(
+    'Not started',
+    'In progress',
+    'Finished'
+);
+
+
 CREATE TABLE TASKS (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(1000) NOT NULL
+    name VARCHAR(1000) NOT NULL,
+    status STATUS NOT NULL DEFAULT 'Not started'
 );
